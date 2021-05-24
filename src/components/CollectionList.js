8a3e45f71771c984cbb2bbf075f31f6587b0b65f
rzhou10 <rzhou10@gmail.com>
2021-05-24 10:40:28 -0400

    update I cannot test for a bit

--- src/components/CollectionList.js
+++ src/components/CollectionList.js
@@ -12,22 +12,38 @@ export default class CollectionList extends Component {
 
   componentDidMount = () => {
     axios.get('https://api.discogs.com/users/ChunkoHead/collection/folders/0/releases?sort=added')
-    .then((response) => {
-      axios.get(response.data.pagination.urls.last)
-      .then((latestUpdates) => {
-        // to do, need to get the last elements of the 2nd to last page if there are less than 10
-        if (latestUpdates.data.releases.length < 10){
-          this.setState({
-            latest: latestUpdates.data.releases
-          });
-        }
-        else{
-          this.setState({
-            latest: latestUpdates.data.releases.slice(-10)
-          });
-        }
-      })}
-    );
+      .then((response) => {
+        axios.get(response.data.pagination.urls.last)
+          .then((latestUpdates) => {
+            if (latestUpdates.data.releases.length < 10){
+              var len = latestUpdates.data.releases.length;
+              this.setState({
+                latest: latestUpdates.data.releases.slice(-len)
+              });
+              this.getLatestPrevious(latestUpdates.data, latestUpdates.data.releases.length);
+            }
+            else{
+              this.setState({
+                latest: latestUpdates.data.releases.slice(-10)
+              });
+            }
+
+            document.getElementById('loadingText').style.visibility = 'hidden';
+          })}
+      );
+  };
+
+  // I need to get the latest from the previous page if there's < 10 entries on the final page
+  getLatestPrevious = (lastUpdate, pageLength) => {
+    var leftover = 10 - pageLength;
+    axios.get(lastUpdate.pagination.urls.prev)
+      .then((response) => {
+        var finalPrev = response.data.releases.slice(-leftover);
+        var oldState = this.state.latest;
+        this.setState({
+          latest: [...finalPrev, ...oldState]
+        })
+      })
   };
 
   // may have multiple labels, so need to include whole list
@@ -45,24 +61,27 @@ export default class CollectionList extends Component {
 
   // Discogs numbers different artists/labels to differntiate ones with the same name, this
   // is to strip that since it's not necessary here
-  stripUniqueNumber(aristOrLabel){
+  stripUniqueNumber = (aristOrLabel) => {
     // eslint-disable-next-line
     return aristOrLabel.replace(/\(([^\)]+)\)/, '').trim();
   }
 
   render = () => {
     return (
-      <div className="row">
-        {
-          this.state.latest.map(album => 
-            <div key={album.id} className='album'>
-              {this.renderArtists(album.basic_information.artists)}
-              <h3>{album.basic_information.title}</h3>
-              <div>{album.basic_information.year === 0 ? 'Unknown' : album.basic_information.year}</div>
-              <div><span id='labelSpan'>Label</span>: {this.renderLabels(album.basic_information.labels)}</div>
-            </div>
-          )
-        }
+      <div>
+        <h1 id='loadingText'>Loading...</h1>
+        <div className='row'>
+          {
+            this.state.latest.map(album => 
+              <div key={album.id} className='album'>
+                {this.renderArtists(album.basic_information.artists)}
+                <h3>{album.basic_information.title}</h3>
+                <div>{album.basic_information.year === 0 ? 'Unknown' : album.basic_information.year}</div>
+                <div><span id='labelSpan'>Label</span>: {this.renderLabels(album.basic_information.labels)}</div>
+              </div>
+            )
+          }
+        </div>
       </div>
     )
   }
